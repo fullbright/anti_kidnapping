f932b85fa4dea9e5ac6e3fc1329350b28c5aed1c
Ihor Bulaiesvkyi <latente.traumgedanken@gmail.com>
2019-11-29 00:43:58 +0200

    reqirements upd

--- model.py
+++ model.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 
-# db_str = 'postgres://agzvarxu:PrM4uplKO3suxsBSts8c57'
-# 'w3h8uxie6X@balarama.db.elephantsql.com:5432/agzvarxu'
-db_str = 'postgres://admin:admin@localhost:5432/antikidnapping'
+db_str = 'postgres://agzvarxu:PrM4uplKO3suxsBSts8c57'\
+         'w3h8uxie6X@balarama.db.elephantsql.com:5432/agzvarxu'
+# db_str = 'postgres://admin:admin@localhost:5432/antikidnapping'
 db = create_engine(db_str)
 
 Base = declarative_base()
